e1189c5052d0c8ba51728313c2902c1260b409dd
BubbleMutter <BubbleMutter@gmail.com>
2024-02-25 00:50:54 +0800

    update: debian desktop

---
+++ 91.package/debian_desktop/apt.desktop.sh
@@ -0,0 +1,34 @@
+# hardware
+apt install nvidia-driver nvidia-detect libspa-0.2-bluetooth
+apt install firmware-iwlwifi firmware-misc-nonfree broadcom-sta-dkms
+apt install hibernate gparted gvfs-fuse gvfs-backends plymouth-themes
+# xfce4
+apt install xfce4 xfce4-goodies network-manager-gnome
+apt install fonts-wqy-microhei fonts-wqy-zenhei ibus-libpinyin
+apt install alacarte lightdm lightdm-gtk-greeter-settings
+apt install xpad uget-integrator autokey-gtk gnome-clocks virtualbox-6.1
+# multi media
+apt install audacious vlc kid3-cli kid3-qt
+apt install flameshot cheese recordmydesktop gnome-sound-recorder
+apt install makemkv-bin makemkv-oss
+apt install wavpack cuetools flac shntool abcde mkcue
+apt install calibre-bin
+# science calculation
+apt install qalculate speedcrunch geogebra
+# network client
+apt install thunderbird teamviewer filezilla vinagre tigervnc-standalone-server
+# commandline misc
+apt install apt-file aptitude snapd unrar-free chrony rsync ssh sshpass
+# development commandline tools
+apt install zsh ranger trash-cli htop tmux rs jq git
+apt install vim vim-asciidoc
+apt install uuid hexcurse global source-highlight pyflakes3
+# development gui tools
+apt install kdiff3-qt doxygen-gui cmake-qt-gui ghex
+apt install cppman zeal manpages-posix-dev susv2 susv3
+# network toolbox
+apt install proxychains4 redsocks v2ray
+apt install mtr atop dstat tcpdump wireshark-gtk
+apt install redis-server npm nginx dnsmasq debian-installer-10-netboot-amd64
+
+snap install firefox xmind wps-2019-snap drawio
